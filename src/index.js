20c55821dc4a2fb084842dd95afe1bb2e1e820ee
Alex <alex.fallen.part@gmail.com>
2018-03-01 01:05:03 +0300

    function added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+    var eq2 = equation.replace(/ /g, '');
+    var eq = eq2.split('*x^2');
+    var a = parseInt(eq[0]);
+    var b = parseInt(eq[1].split('*x')[0]);
+    var c = parseInt(eq[1].split('*x')[1]);
+
+    var dis = Math.pow(b, 2) - 4 * a * c;
+
+    var x1 = Math.round((-b + Math.sqrt(dis)) / (2 * a));
+    var x2 = Math.round((-b - Math.sqrt(dis)) / (2 * a));
+    if (x1 >= x2) {
+        return [x2, x1]
+    } else {
+        return [x1, x2]
+    }
+
 }
